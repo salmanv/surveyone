562bf4173a1148667786b56ce11c93926108deb2
Salman Vajid <s@vajid.com>
2013-11-12 11:43:00 -0500

    adding users half

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Surveyone::Application.routes.draw do
-  get "users/new"
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup', to: 'users#new', via: [:get, :post]
+  match '/signin', to: 'sessions#new', via: [:get, :post]
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+
+  # get "users/new"
   mount Surveyor::Engine => "/surveys", :as => "surveyor"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
